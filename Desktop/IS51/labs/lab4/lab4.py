70b8ce2483a93f6a0f7a19ceab063dfbf1230806
chrism <chrism@kingorange.com>
2021-05-07 14:33:21 -0700

    structured english

---
+++ Desktop/IS51/labs/lab4/lab4.py
@@ -0,0 +1,17 @@
+
+
+
+
+"""
+this program allows users three tries to guess the correct answer to a question
+"what is the capitol of california" answer = Sacramento
+
+
+set max tries to 3 then we create a loop to iterate them three times
+in each iteration we ask the user for an answer and then check to see if it is correct.
+if the answer is correct, print "Correct!" and rterminate the program.
+
+if the user could not get the answer correct then print "you have used up all 3 of your chances" and termiate the program
+
+
+"""
